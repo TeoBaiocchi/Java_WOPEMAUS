f431944001aa342ddefe3ca27a7cecb400323acb
Teo Baiocchi <teobaiocchiensinck@gmail.com>
2024-03-27 21:34:49 -0300

    Commit inicial

---
+++ src/test/java/com/teo/_AUS_ProyectoWeb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teo._AUS_ProyectoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
