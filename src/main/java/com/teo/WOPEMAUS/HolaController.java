95b661061a641b654d2d658d8196f8a3c7738243
Teo Baiocchi <teobaiocchiensinck@gmail.com>
2024-04-03 22:03:59 -0300

    Avances clase nro2

--- src/main/java/com/teo/WOPEMAUS/HolaController.java
+++ src/main/java/com/teo/WOPEMAUS/HolaController.java
@@ -4,7 +4,10 @@
  */
 package com.teo.WOPEMAUS;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +16,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("wopemaus")
 public class HolaController {
    
+    @Autowired
+    UsuarioRepository userSrv;
+    
     @GetMapping("/hola")
     public String hello(){
         return "hola";
     }
+    
+    @GetMapping("/hardcodeado")
+    public String crearUsuariosHardcodeado(){
+        Usuario usr = new Usuario();
+        usr.setUsername("andres");
+        userSrv.save(usr);
+        
+        Usuario usr2 = new Usuario();
+        usr2.setUsername("teo");
+        userSrv.save(usr2);
+  
+        return "Guardado con exito";
+    }
+    
+    @GetMapping("/listarUsuarios")
+    public String listarUsuarios(){
+
+        String listado = "Los usuario son: \n";
+        for(Usuario usuario : userSrv.findAll()){
+            listado = listado + usuario.getUsername() + "\n";
+        }
+        
+        return listado;
+    }
+    
+    @PostMapping("usuarios")
+    public Usuario saveUser(@RequestBody Usuario user){
+        return userSrv.save(user);
+    }
+    
+    //curl http://172.20.224.1:8080/wopemaus/usuarios -H 'Content-Type: application/json' -d '{"username":"luciano","password":"lol","email":"luciano@gmail"}'
     
 }
---
+++ src/main/java/com/teo/WOPEMAUS/Usuario.java
@@ -0,0 +1,23 @@
+package com.teo.WOPEMAUS;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String username;
+    private String password;
+    private String email;
+    private boolean enabled;
+    
+}
